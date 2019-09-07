6ec77bc0f14a35e7f6c1cba4bdefdb7b46114584
Mykola Zaiarnyi <zayarnyi.m@gmail.com>
2019-09-07 16:12:06 +0300

    Added abstraction

---
+++ DataLayer/DataLayer/Abstraction/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Abstraction {
+    public interface IEntity <TKey>{
+        TKey Id { get; set; }
+    }
+}
---
+++ DataLayer/DataLayer/Abstraction/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Abstraction {
+    public interface IRepository <TEntity, TKey> where TEntity: IEntity<TKey> {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(TKey id);
+        Task CreateAsync(TEntity item);
+        Task DeleteAsync(TKey id);
+        Task UpdateAsync(TEntity item);
+    }
+}
