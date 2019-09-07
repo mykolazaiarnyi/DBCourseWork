cd0f015bc747dfd2e377c8a686608f7a241fbd90
Mykola Zaiarnyi <zayarnyi.m@gmail.com>
2019-09-07 19:57:24 +0300

    Added hierarchy

--- DBCourseWork/DataLayer/Entities/Payment.cs
+++ DBCourseWork/DataLayer/Entities/Payment.cs
@@ -4,12 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace DataLayer.Entities {
-    public class Payment : IEntity<int> {
-        public int Id { get; set; }
-        public DateTime Time { get; set; }
-        public int FromUserId { get; set; }
+    public class Payment : Expense {
         public int ToUserId { get; set; }
-        public int GroupId { get; set; }
-        public decimal Amount { get; set; }
     }
 }
