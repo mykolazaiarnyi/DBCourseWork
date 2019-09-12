e7f4077801f5d9f1fc5c42597f6f105e9f2a2a92
Mykola Zaiarnyi <zayarnyi.m@gmail.com>
2019-09-12 10:51:43 +0300

    Added function get_user_balance

--- DBCourseWork/DataLayer/Queries/Functions.sql
+++ DBCourseWork/DataLayer/Queries/Functions.sql
@@ -19,3 +19,36 @@ return (
 		where ug.group_id = @id
 )
 go
+
+create function get_users_balance (@user_id_1 int, @user_id_2 int, @group_id int)
+returns money
+as
+begin
+	declare @number_of_members int
+	select @number_of_members = count(*) from user_groups as ug
+		where ug.group_id = @group_id
+
+	declare @spent_by_1 money
+	select @spent_by_1 = (sum(e.amount) / (@number_of_members - 1)) from expenses as e 
+		where e.group_id = @group_id
+			and e.[user_id] = @user_id_1
+
+	declare @spent_by_2 money
+	select @spent_by_2 = (sum(e.amount) / (@number_of_members - 1)) from expenses as e 
+		where e.group_id = @group_id 
+			and e.[user_id] = @user_id_2
+
+	declare @payed_by_1 money
+	select @payed_by_1 = sum(p.amount) from payments as p
+		where p.[user_id] = @user_id_1 
+			and p.to_user_id = @user_id_2 
+			and p.group_id = @group_id
+
+	declare @payed_by_2 money
+	select @payed_by_2 = sum(p.amount) from payments as p
+		where p.[user_id] = @user_id_2 
+			and p.to_user_id = @user_id_1 
+			and p.group_id = @group_id
+
+	return (@spent_by_1 - @payed_by_2) - (@spent_by_2 - @payed_by_1)
+end
